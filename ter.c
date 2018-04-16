9207003b7f7398fd29755b4c08a084cc255c8960
Viaaaa <vianney.tessier@orange.fr>
2018-04-16 09:05:26 +0200

    stockageregles

--- ter.c
+++ ter.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "string.h"
+#include <string.h>
 
 
 typedef struct environnement environnement;
@@ -24,9 +24,9 @@ struct agent{
 
 //Regle
 struct regle{
-	int* polarite_gauche;
+	int negatif_gauche[10];
 	agent* agents_gauche[10];
-	int polarite_droite;
+	int negatif_droite[10];
 	agent* agents_droite[10];
 };
 
@@ -39,7 +39,7 @@ environnement recuperer_environnement(char* nom_fichier){
     	printf("Impossible d'ouvrir le fichier\n");
     	exit(EXIT_FAILURE);
     }
-    int ou = 0;
+    int i,ou = 0;
     char c;
     struct environnement env;
     env.nombre_agents = 0;
@@ -69,9 +69,11 @@ environnement recuperer_environnement(char* nom_fichier){
     rewind(fichier);    
     //printf("NbA: %d, NbR: %d\n", env.nombre_agents, env.nombre_regles);
 
-	//On stocke les agents et regles
+	//On stocke les agents
 	int compteur_agents = 0, dans_agents = 0;
     env.agents = malloc(env.nombre_agents*sizeof(agent));
+    env.regles = malloc(env.nombre_regles*sizeof(regle));
+    
     do 	{
   		c = fgetc(fichier);
  		if(ou < 6 && (c == '+' || c == '-')) ou = -1;	
@@ -97,13 +99,75 @@ environnement recuperer_environnement(char* nom_fichier){
  		else if(c == 'e' && ou == 3) ou++;
  		else if(c == 's' && ou == 4) ou++;
  		else if(c == ':' && ou == 5) ou++;
- 		else if(c == '>' && ou == 6) ou++;
- 		else if(ou == 7){
- 			//if(c == '>') env.nombre_regles++;
- 			ou = 6;		
- 		}
- 		else if(ou < 6) ou = 0;
+    } while (ou != 6);
+    
+    //On stocke les regles
+    ou = 0;
+    dans_agents = 0;
+    char temp[5];
+    int dans_regles, compteur_regles = 0;
+    do{
+    	c = fgetc(fichier);
+    	if(c == '>' & (ou == 0 || ou == 1)) ou++;
+    	if(ou == 2){
+    		do{
+ 				fseek(fichier, -2, SEEK_CUR);
+ 				c = fgetc(fichier);
+ 			} while (c != '\n');
+ 			
+ 			//gauche
+ 			do{
+ 				c = fgetc(fichier);
+ 				if(c == ','){
+ 					dans_regles++;
+ 					dans_agents = 0;
+ 				}
+ 				else if((c >= 65 && c <= 90) || (c >= 97 && c >= 122)){
+ 					do{
+ 						temp[dans_agents] = c;
+ 						dans_agents++;
+ 						c = fgetc(fichier);	
+ 					} while(c != '+' && c != '-');
+ 					if(c == '+') env.regles[compteur_regles].negatif_gauche[dans_regles] = 0;
+ 					else if(c == '-') env.regles[compteur_regles].negatif_gauche[dans_regles] = 1;
+ 					for(i = 0; i < env.nombre_agents; i++){
+ 						if(strcmp(env.agents[i].nom, temp)==0) env.regles[compteur_regles].agents_gauche[dans_regles] = &env.agents[i];
+ 					}
+ 					
+ 				}
+ 				
+ 			} while (c != '>');
+ 			fseek(fichier, 1, SEEK_CUR);
+ 			dans_regles=0;
+ 			//droite
+ 			do{
+ 				c = fgetc(fichier);
+ 				if(c == ','){
+ 					dans_regles++;
+ 					dans_agents = 0;
+ 				}
+ 				else if((c >= 65 && c <= 90) || (c >= 97 && c >= 122)){
+ 					do{
+ 						temp[dans_agents] = c;
+ 						dans_agents++;
+ 						c = fgetc(fichier);	
+ 					} while(c != '+' && c != '-');
+ 					if(c == '+') env.regles[compteur_regles].negatif_droite[dans_regles] = 0;
+ 					else if(c == '-') env.regles[compteur_regles].negatif_droite[dans_regles] = 1;
+ 					for(i = 0; i < env.nombre_agents; i++){
+ 						if(strcmp(env.agents[i].nom, temp)==0) env.regles[compteur_regles].agents_droite[dans_regles] = &env.agents[i];
+ 					}
+ 					
+ 				}
+ 				
+ 			} while (c != '\n');
+ 			
+ 			compteur_regles++;
+ 			ou = 0;	
+    	}
+    
     } while (c != EOF);
+    
     
     
     fclose(fichier);
@@ -131,7 +195,8 @@ int main(int argc, char* argv[]){
 	environnement env1 = recuperer_environnement(argv[1]);
 	
 	printf("%c %c %c\n", env1.agents[0].nom[0], env1.agents[0].nom[1], env1.agents[0].nom[4]);
-	
+	printf("%d\n", env1.regles[0].negatif_droite[2]);
+	//printf("%c %d\n", env1.regles[0].agents_gauche[0]->nom[0], env1.regles[0].negatif_gauche[0]);
 	//environnement env2 = recuperer_environnement(argv[2]);
 	
 	
